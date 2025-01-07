1a6fecd4942adfcee3342d3a5ee697f87e10e14a
Mudassir Chapra <muddi900@gmail.com>
2025-01-07 10:24:25 -0500

    Done for now.

--- 1769/1769.py
+++ 1769/1769.py
@@ -11,14 +11,13 @@ class Solution:
         while i < j:
             if boxes[i] == "1":
                 answer[j] += j - i
-                if j < length - 1:
-                    answer[j+1] += answer[j] + 1
+            
+            answer[j-1] += answer[j] - 1
                     
 
             if boxes[j] == "1":
                 answer[i] += j - i
-                if i > 0:
-                    answer[i-1] = answer[i] + 1
+                answer[i + 1] += answer[i] - 1
 
             i += 1
             j -= 1
@@ -31,5 +30,8 @@ class Solution:
 s = Solution()
 print(s.minOperations("001011"))
 
+
 # Brute Force solution(0fe98036bb0108e2c49c13fbf100ad5904d98759): Worked but the result was at the 22.84%
 # Two point solution(e36e483b82969a843e97ab7e75788d1933fd00fe): does not work as implemented. The pointers are quite naive.
+# Another stupid approach in d408dfc3e519a06c25afa981ee72f72f0d3a1339.
+# I have the right idea but missing something. Creating a PR for now.
