07dd0952903a69342cbe901ebcb503b9d274f30a
Mudassir Chapra <muddi900@gmail.com>
2025-01-07 12:01:44 -0500

    Notes are in Markdown now.

--- 1408/1408.py
+++ 1408/1408.py
@@ -15,12 +15,6 @@ class Solution:
         return list(answer)
 
 
-# HashMap based solution solves all test cases(e1cafae21233179e285135c861ffc747f3988454).
-# Solution is fairly fast but still at 93 percentile(3d6640f93a7c515a3f4fe8bd3dccc6672ef710a4). Clearly I am being naive somewhere.
-# The issue seems to be actual string matching, which is very slow.
-# Sorting fixes the speed issue. The approach as not changed but sorting
-
-
 s = Solution()
 print(s.stringMatching(["mass", "as", "hero", "superhero"]))
 print(s.stringMatching(["leetcode", "et", "code"]))
