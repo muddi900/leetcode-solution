276ca8ea8dcef053c3691e9a04cbaa61d64cb82c
Mudassir Chapra <muddi900@gmail.com>
2025-01-09 14:21:51 -0500

    Created a brutforce solution for 2145[Issue #15]

---
+++ 2145/python/main.py
@@ -0,0 +1,22 @@
+from typing import *
+
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        count = 0
+
+        for word in words:
+            if word == pref:
+                count += 1
+                continue
+
+            if word[: len(pref)] == pref:
+                count += 1
+
+        return count
+
+
+s = Solution()
+
+print(s.prefixCount(["pay", "attention", "practice", "attend"], "at"))
+print(s.prefixCount(words=["leetcode", "win", "loops", "success"], pref="code"))
